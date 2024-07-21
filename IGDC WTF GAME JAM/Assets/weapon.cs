58f404b7c73f2069e598365a1af5986f94c4975b
mangokiddy <120718300+mangokiddy@users.noreply.github.com>
2024-07-21 13:01:31 +0530

    asd

--- IGDC WTF GAME JAM/Assets/weapon.cs
+++ IGDC WTF GAME JAM/Assets/weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class weapon : MonoBehaviour
 {
     // Start is called before the first frame update
+    public MeshCollider collider;
     void Start()
     {
         
@@ -14,5 +15,13 @@ public class weapon : MonoBehaviour
     void Update()
     {
         
+    }
+    public void EnableCollider()
+    {
+        collider.enabled = true;
+    }
+    public void DisableCollider()
+    {
+        collider.enabled = false;
     }
 }
